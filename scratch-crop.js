3f26a174f82113637bfad8727a7b41c456699b50
tranxuannhat88-cyber <tranxuannhat88@gmail.com>
2026-09-01 09:11:42 +0700

    ngu

---
+++ scratch-crop.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+
+// Let's inspect packages or use canvas / sharp or a simple pure JS PNG reader/cropper
+console.log("Ready to crop user images");
